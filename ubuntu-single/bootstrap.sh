5be7f9c141a68bf5332618d5c3442db42fa11c00
Attila Kanto <attila.kanto@gmail.com>
2014-08-13 11:40:32 +0200

    change the docker installation mode

--- ubuntu-single/bootstrap.sh
+++ ubuntu-single/bootstrap.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y bridge-utils
+apt-get install -y bridge-utils nscd
 
 #Docker installation
 if [ ! -f "/var/docker_setup" ]; then
   echo "Install docker"
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-  sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-  apt-get update
-  apt-get install -y lxc-docker
+  curl -sL https://get.docker.io/ | sh
 
   docker pull ubuntu
   
