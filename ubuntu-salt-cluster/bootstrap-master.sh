a537d5638bbefead15affc6ae012374002b81ac6
Attila Kanto <attila.kanto@gmail.com>
2015-02-15 02:24:02 +0100

    Add UI installation to saltstack

--- ubuntu-salt-cluster/bootstrap-master.sh
+++ ubuntu-salt-cluster/bootstrap-master.sh
@@ -13,3 +13,24 @@ if [ ! -f "/var/salt_setup" ]; then
 
   touch /var/salt_setup
 fi
+
+#touch /var/halite_setup
+#Halite setup
+if [ ! -f "/var/halite_setup" ]; then
+  echo "Halite setup"
+
+  #http://docs.saltstack.com/en/latest/topics/tutorials/halite.html
+  apt-get install -y gcc python-pip python-dev libevent-dev
+
+  pip install -U halite
+  pip install CherryPy
+  pip install gevent
+  pip install pyopenssl
+
+  salt-call tls.create_self_signed_cert tls
+
+  cat /vagrant/halite.yml >> /etc/salt/master
+  service salt-master restart
+
+  touch /var/halite_setup
+fi
