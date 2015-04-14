3bf721f4f900f023d75d49e7f90c7d0dcd5e1771
Attila Kanto <attila.kanto@gmail.com>
2015-04-15 01:24:31 +0200

    fix  heredoc and do not start the cbd automatically

--- cbd-box/bootstrap.sh
+++ cbd-box/bootstrap.sh
@@ -55,19 +55,18 @@ install_cloudbreak() {
   mkdir /root/cbd_test
   cd /root/cbd_test
   cat > Profile << ENDOFPROFILE
-  export PRIVATE_IP=$(docker run alpine sh -c 'ip ro | grep default | cut -d" " -f 3')
-  export PUBLIC_IP=192.168.44.10
-  export CLOUDBREAK_SMTP_SENDER_USERNAME=xxx
-  export CLOUDBREAK_SMTP_SENDER_PASSWORD=xxx
-  export CLOUDBREAK_SMTP_SENDER_HOST=email-smtp.us-east-1.amazonaws.com
-  export CLOUDBREAK_SMTP_SENDER_PORT=25
-  export CLOUDBREAK_SMTP_SENDER_FROM=dummy@example.com
-  export UAA_CLOUDBREAK_SECRET=b6bffc749b
-  export UAA_PERISCOPE_SECRET=b6bffc749b
-  export UAA_ULUWATU_SECRET=b6bffc749b
-  export UAA_SULTANS_SECRET=b6bffc749b
-  ENDOFPROFILE
-  cbd start
+export PRIVATE_IP=$(docker run alpine sh -c 'ip ro | grep default | cut -d" " -f 3')
+export PUBLIC_IP=192.168.44.10
+export CLOUDBREAK_SMTP_SENDER_USERNAME=xxx
+export CLOUDBREAK_SMTP_SENDER_PASSWORD=xxx
+export CLOUDBREAK_SMTP_SENDER_HOST=email-smtp.us-east-1.amazonaws.com
+export CLOUDBREAK_SMTP_SENDER_PORT=25
+export CLOUDBREAK_SMTP_SENDER_FROM=dummy@example.com
+export UAA_CLOUDBREAK_SECRET=b6bffc749b
+export UAA_PERISCOPE_SECRET=b6bffc749b
+export UAA_ULUWATU_SECRET=b6bffc749b
+export UAA_SULTANS_SECRET=b6bffc749b
+ENDOFPROFILE
 }
 
 
