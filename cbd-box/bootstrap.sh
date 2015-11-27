38ebdff8335ccdaa9b0ff3302991b61e3cbf3c1b
Attila Kanto <attila.kanto@gmail.com>
2015-11-27 10:47:09 +0100

    Fic invalid heredoc

--- cbd-box/bootstrap.sh
+++ cbd-box/bootstrap.sh
@@ -23,7 +23,7 @@ install_docker() {
   cat >/etc/yum.repos.d/docker.repo <<-EOF
 [dockerrepo]
 name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
 enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
